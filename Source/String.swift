d1ce5097f31e7c4eb787d4151eb5820436797098
David Bagwell <dmbagwell@hotmail.com>
2021-02-25 15:33:12 -0600

    - Added some convenience methods for getting string ranges.

--- Source/String.swift
+++ Source/String.swift
@@ -28,6 +28,27 @@ extension String {
         return self.isEmpty ? nil : self
     }
     
+    public func indices(of occurrence: String) -> [Int] {
+        var indices = [Int]()
+        var position = startIndex
+        
+        while let range = range(of: occurrence, range: position..<endIndex) {
+            let i = distance(from: startIndex, to: range.lowerBound)
+            indices.append(i)
+            let offset = occurrence.distance(from: occurrence.startIndex, to: occurrence.endIndex) - 1
+            guard let after = index(range.lowerBound, offsetBy: offset, limitedBy: endIndex) else { break }
+            position = index(after: after)
+        }
+        
+        return indices
+    }
+    
+    public func ranges(of searchString: String) -> [Range<String.Index>] {
+        let indices = self.indices(of: searchString)
+        let count = searchString.utf16.count
+        return indices.map({ self.index(startIndex, offsetBy: $0)..<self.index(startIndex, offsetBy: $0 + count) })
+    }
+    
     public func matches(pattern: String) -> Bool {
         guard let regex = try? NSRegularExpression(pattern: pattern) else { return false }
         let range = NSRange(location: 0, length: self.utf16.count)
